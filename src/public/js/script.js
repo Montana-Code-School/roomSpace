2cc5c0b3befc94af689fd8031cac96f9aec644ad
John Lee <lee121@gmail.com>
2017-05-28 12:08:08 -0600

    updates

--- src/public/js/script.js
+++ src/public/js/script.js
@@ -1,1 +1,10 @@
-alert('LOADED');
+
+var width = 500;
+var height = 500;
+var radius = Math.min(width, height)/2;
+
+var chartdata = [54,954,654,8,47,654,65,6,8,4,654,654,65,4];
+
+var chart = d3.select(".chart");
+
+d3.select("#bar-chart").append("svg")
