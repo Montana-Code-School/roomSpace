43de71daeb8a3f45bca37d284f3f6cc59e7ddacd
kashyab <kashyaboretsky@gmail.com>
2017-05-30 20:50:35 -0600

    working d3 bar graph

--- src/app/app.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import RoomSpace from './roomspace'
-
-ReactDOM.render(<RoomSpace />, document.getElementById('chart'));
--- src/public/js/script.js
+++ src/public/js/script.js
@@ -1,10 +1,27 @@
+//
+var chartData = [10,20,30,40,50,60,70,80];
 
-var width = 500;
-var height = 500;
-var radius = Math.min(width, height)/2;
+var height = 200,
+  width = 720,
+  barWidth = 40,
+  barOffset = 20;
 
-var chartdata = [54,954,654,8,47,654,65,6,8,4,654,654,65,4];
+var chart = d3.select('#bar-chart');
+var group = chart.append('svg').attr('width', width).attr('height', height).selectAll('rect').data(chartData).enter().append("rect")
+  group.each(function(data, index) {
+    d3.select(this).attr('height', data);
+    d3.select(this).attr('width', barWidth);
 
-var chart = d3.select(".chart");
+    d3.select(this).attr("fill", "red");
+    d3.select(this).attr('x', index * (barWidth + barOffset));
+    d3.select(this).attr('y', height - data);
+  })
 
-d3.select("#bar-chart").append("svg")
+    // .attr('height', function(chartData) {
+    //   console.log(chartData);
+    //   return chartData;
+    // })
+    // .attr(
+    // .attr('y', function(chartData) {
+    //   return height - chartData;
+    // });
