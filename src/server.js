f4d739e039aa8dc4fd0f3e54bd746b0635504f6a
ed42311 <ed42311@gmail.com>
2017-07-06 20:25:28 -0600

    testing heroku

--- src/server.js
+++ src/server.js
@@ -2,10 +2,11 @@ import express from 'express';
 import path from 'path';
 
 const app = express();
+const port = process.env.PORT || 3000
 app.use(express.static('src/public'));
 
 app.get('/',(req,res)=>{
   res.sendFile('index.html');
 });
 
-app.listen(3000,()=>console.log("listening on port 3000"));
+app.listen(port,()=>console.log("listening on port 3000"));
